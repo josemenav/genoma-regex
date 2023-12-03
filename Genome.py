8fc403e7de36c69f2e9da99e6abdb80c9c29cc42
Eduardo Javier García López <eduardo.lalo1999@gmail.com>
2023-12-03 00:59:57 -0600

    Refactor Genoma class and add validation, motif search, and structure identification functions
    Signed-off-by: Eduardo Javier García López <eduardo.lalo1999@gmail.com>

--- Genome.py
+++ Genome.py
@@ -3,12 +3,7 @@ import re
 import data
 
 
-def validate(adn):
-    valid = re.compile(r"^(.{3})*$")
-    return bool(valid.match(adn))
-
-
-class Genoma:
+class Genome:
     def __init__(self, adn):
         self.adn = adn
         self.size = len(adn)
@@ -25,10 +20,9 @@ class Genoma:
 
     def to_arn(self):
         self.arn = self.adn.replace('T', 'U')
-        valid = validate(self.arn)
-        if valid:
+        if self.validate(self.arn):
             for i in range(0, self.size, 3):
-                codon = self.arn[i:i+3]
+                codon = self.arn[i:i + 3]
                 self.codons.append(codon)
             return True
         else:
@@ -44,6 +38,11 @@ class Genoma:
                 print(f"El codon {codon} no es válido")
                 return False
         return True
+
+    @staticmethod
+    def validate(x):
+        valid = re.compile(r"^(.{3})*$")
+        return bool(valid.match(x))
 
 
 def find_motif(genome, motif):
@@ -68,41 +67,3 @@ def identify_rna_structure(genome):
     pattern = re.compile(r"([GC]{3,}|[AU]{3,})")
     matches = pattern.findall(genome.arn)
     print(matches)
-
-
-def main():
-    print("Secuencia de ADN:")
-    genomastr = input("Escribe la secuencia de ADN a analizar o deje en blanco para usar una predeterminada: ")
-
-    if len(genomastr) < 1:
-        genoma = Genoma(
-            "ATGGATCCAAGTATGGGTGTGAATTCTGTTACCATTTCTGTTGAGGGTATGACTTGCAATTCCTGTGTTTGGACCATTGAGCAGCAGATTGGAAAAGTGAATGGTGTGCATCACATTAAGGTATCACTGGAAGAAAAA")
-    else:
-        genoma = Genoma(genomastr)
-
-    print("Secuencia de ADN:", genoma.adn)
-    print("Tamaño:", genoma.size)
-    print("ARN:", genoma.arn)
-    print("Codones:", genoma.codons)
-    print("Proteínas:", genoma.proteins)
-    print("")
-
-    # Ejemplo de busqueda de motif
-    print("Busqueda de motif:")
-    motif = "ATG"
-    print("Motif:", motif)
-    find_motif(genoma, motif)
-    print("")
-
-    # Ejemplo de identificación de estructura de proteínas
-    print("Identificación de estructura de proteínas:")
-    identify_protein_structure(genoma)
-    print("")
-
-    # Ejemplo de identificación de estructura de ARN
-    print("ARN:", genoma.arn)
-    identify_rna_structure(genoma)
-
-
-if __name__ == "__main__":
-    main()
